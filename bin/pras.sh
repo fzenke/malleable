61cf9a5f50e6fad6957dce3cf6eecb3134d93a52
Friedemann Zenke <fzenke@stanford.edu>
2015-09-01 14:46:34 -0700

    Adds support to display bras files

--- bin/pras.sh
+++ bin/pras.sh
@@ -8,12 +8,16 @@
 # Accepts a second file too e.g.:
 # pras.sh excitatory.0.ras inhibitory.0.ras
 
-NSPIKES=20000
-
 if [ "$1" != "" ]; then
 	FILE=$1
 else
-	FILE=`ls -tr1 *.ras | tail -n 1`
+	FILE=`ls -tr1 *ras | tail -n 1`
+fi
+
+if [[ $FILE =~ \.bras$ ]]; then
+	EXTBIN="aube --last 10 --inputs "
+else
+	EXTBIN="peep.sh"
 fi
 
 
@@ -30,21 +34,21 @@ set xtics nomirror out
 set ytics nomirror out
 set xlabel 'Time [s]'
 set ylabel 'Neuron ID'
-plot '< peep.sh $FILE $NSPIKES' with dots lc -1
+plot '< $EXTBIN $FILE' with dots lc -1
 unset out
 __EOF
 	else
 		FILE2=$2
 cat << __EOF | gnuplot -p 
 set xlabel 'Time [s]'
-plot '< peep.sh $FILE $NSPIKES' with dots lc -1,\
-	'< peep.sh $FILE2 $NSPIKES' using 1:(-column(2)) with dots lc 3 
+plot '< $EXTBIN $FILE' with dots lc -1,\
+	'< $EXTBIN $FILE2' using 1:(-column(2)) with dots lc 3 
 __EOF
 	fi
 	else
 cat << __EOF | gnuplot -p 
 set xlabel 'Time [s]'
-plot '< peep.sh $FILE $NSPIKES' with dots lc -1
+plot '< $EXTBIN $FILE' with dots lc -1
 __EOF
 fi
 
