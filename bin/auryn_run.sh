758dabdf171d441fd28324344c0ed560056eb602
Friedemann Zenke <fzenke@stanford.edu>
2016-08-22 10:02:54 -0700

    Adds auryn_run.sh

--- bin/auryn_run.sh
+++ bin/auryn_run.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+# Script that 'make compiles' and then runs a given Auryn simulation binary
 DIR=$(dirname $1)
 BIN=$(basename -s .cpp $1)
+ARGS="${@:2}"
 echo "Auryn run of $BIN in dir=$DIR started at $(date)" 
-make -C $DIR $BIN && ./$BIN
+echo "With args $ARGS"
+make -C $DIR $BIN && ./$BIN $ARGS
